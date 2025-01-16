eccc060bb5d7226905ffba1a4535c3e90e5290f6
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-01-16 17:23:59 -0500

    added new components

---
+++ src/client/components/DisplayError.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const DisplayError = () => {
+  // input logic here!
+
+  return (
+    <div>
+    <h2>I'm display an error here</h2>
+    <h3>I'm also displaying another error</h3>
+    </div>
+  )
+}
+
+export default DisplayError;
