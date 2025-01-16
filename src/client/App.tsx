f6e73fc5d9e684fa148f7be171089c6f48af55da
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-01-16 17:37:03 -0500

    adjusted rendering components, added new ones for setup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import DisplayError from './components/DisplayError';
 
 const App = () => {
   return (
     <div>
       <h1>Hello App</h1>
+      {/* <insert other components> */}
+    <DisplayError />
     </div>
   );
 };
