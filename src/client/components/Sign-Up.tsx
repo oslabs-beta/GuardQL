b582ffc74bcf1cd4fab1642eaec2033a90ec5226
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-01-22 12:43:38 -0500

    removed inline component stylings

--- src/client/components/Sign-Up.tsx
+++ src/client/components/Sign-Up.tsx
@@ -1,9 +1,3 @@
-// import React from 'react';
-// import Login from './Login';
-
-// const SignUp = () => {};
-
-// export default SignUp;
 import Login from './Login';
 
 import * as React from 'react';
@@ -19,51 +13,7 @@ import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
-import MuiCard from '@mui/material/Card';
-import { styled } from '@mui/material/styles';
-
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignSelf: 'center',
-  width: '100%',
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: 'auto',
-  boxShadow:
-    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-  [theme.breakpoints.up('sm')]: {
-    width: '450px',
-  },
-  ...theme.applyStyles('dark', {
-    boxShadow:
-      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
-  }),
-}));
-
-const SignUpContainer = styled(Stack)(({ theme }) => ({
-  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
-  minHeight: '100%',
-  padding: theme.spacing(2),
-  [theme.breakpoints.up('sm')]: {
-    padding: theme.spacing(4),
-  },
-  '&::before': {
-    content: '""',
-    display: 'block',
-    position: 'absolute',
-    zIndex: -1,
-    inset: 0,
-    backgroundImage:
-      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-    backgroundRepeat: 'no-repeat',
-    ...theme.applyStyles('dark', {
-      backgroundImage:
-        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-    }),
-  },
-}));
+import Card from '@mui/material/Card';
 
 export default function SignUp(props: { disableCustomTheme?: boolean }) {
   const [emailError, setEmailError] = React.useState(false);
@@ -127,8 +77,8 @@ export default function SignUp(props: { disableCustomTheme?: boolean }) {
   return (
     <div>
       <CssBaseline enableColorScheme />
-      <SignUpContainer direction='column' justifyContent='space-between'>
-        <Card variant='outlined'>
+      <Card>
+        <Stack>
           <Typography
             component='h1'
             variant='h4'
@@ -211,8 +161,8 @@ export default function SignUp(props: { disableCustomTheme?: boolean }) {
               </Link>
             </Typography>
           </Box>
-        </Card>
-      </SignUpContainer>
+        </Stack>
+      </Card>
     </div>
   );
 }
